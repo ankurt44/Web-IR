ea1c001c84ccd920b864226f3e1a9b84ab19ca15
Ank <ank@wlan-199043.nbw.tue.nl>
2015-10-15 18:06:42 +0200

    updated

--- src/me/jhenrique/main/Main.java
+++ src/me/jhenrique/main/Main.java
@@ -7,6 +7,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main {
+	private static String PATH = "/Users/ank/Documents/Q1WebIR/twitterData";
 	public static void main(String[] args) {
 //		Tweet t = TweetManager.getTweets(null, "2015-01-01", "2015-01-02", "التعليم الجامعى").get(0);
 //		Tweet t = TweetManager.getTweets("jimmyfallon", "2015-01-01", "2015-01-02", null).get(0);
@@ -14,10 +15,13 @@ public class Main {
 		/*
 		 * Give output filename
 		 */
-		String filename = "test.csv"; 
-		List<Tweet> tweets = TweetManager.getTweets(null, "2015-09-01", "2015-09-05", "allindiabakchod");		
+		String filename = PATH + "TweetsObama.csv"; 
+		System.out.println("Getting tweets.. Wait..");
+		List<Tweet> tweets = TweetManager.getTweets(null, "2015-09-01", "2015-09-02", "BarackObama");
+		System.out.println("Tweets retrieved!!");
+		System.out.println("Writing them to CSV file.");
 		createCSV(filename, tweets);
-		
+		System.out.println("Tweets written to CSV file at lovation:"+PATH+" and filename is:"+filename);
 		/*
 		for(Tweet t : tweets){
 			//if(t.getRetweets()!=0){
@@ -32,10 +36,24 @@ public class Main {
 		FileWriter fileWriter = null;
 		try{
 			fileWriter = new FileWriter(filename);
+			fileWriter.append("UserName");
+			fileWriter.append(SEMI_COLON_SEPERATOR);
+			fileWriter.append("Date");
+			fileWriter.append(SEMI_COLON_SEPERATOR);
+			fileWriter.append("Favorites");
+			fileWriter.append(SEMI_COLON_SEPERATOR);
+			fileWriter.append("Retweets");
+			fileWriter.append(SEMI_COLON_SEPERATOR);
+			fileWriter.append("Text");
+			fileWriter.append(NEW_LINE_SEPARATOR);
 			for(Tweet tweet: tweets){
 				fileWriter.append(tweet.getUsername());
 				fileWriter.append(SEMI_COLON_SEPERATOR);
 				fileWriter.append(tweet.getDate().toString());
+				fileWriter.append(SEMI_COLON_SEPERATOR);
+				fileWriter.append(Integer.toString(tweet.getFavorites()));
+				fileWriter.append(SEMI_COLON_SEPERATOR);
+				fileWriter.append(Integer.toString(tweet.getRetweets()));
 				fileWriter.append(SEMI_COLON_SEPERATOR);
 				fileWriter.append(tweet.getText());
 				fileWriter.append(NEW_LINE_SEPARATOR);
